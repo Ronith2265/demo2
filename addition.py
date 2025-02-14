72f984ddcffe9f7bc3cd4afe3cc6808e2826dd0d
Ronith2265 <kronithakshay@gmail.com>
2025-02-14 15:41:51 +0530

    <first>

--- addition.py
+++ addition.py
@@ -1,13 +1,14 @@
 # Function to add two numbers
-def add_numbers(a, b):
-    return a + b
+def add_numbers(a, b,c):
+    return a + b +c
 
 # Input from user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
 
 # Compute sum
-sum_result = add_numbers(num1, num2)
+sum_result = add_numbers(num1, num2,num3)
 
 # Display result
-print(f"The sum of {num1} and {num2} is {sum_result}")
+print(f"The sum of {num1} and {num2} and {num3} is {sum_result}")
